e0ce48f09920f32b5b9540e91b4545ff69a0dba5
johnny2smooth <johnbp13@gmail.com>
2023-01-11 15:07:58 -0600

    db result parser getting refined to pull important properties

--- src/utils/asyncGetters.ts
+++ src/utils/asyncGetters.ts
@@ -1,13 +1,12 @@
 import { Client } from "@notionhq/client";
 import dotenv from "dotenv";
+import { parseDBInfo } from "./resultParsers";
 
 dotenv.config();
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
-
-import { parseDBInfo } from "./resultParsers";
 
 export async function getDBInfo(database_id: string) {
   const result = await notion.databases.query({
--- src/utils/resultParsers.ts
+++ src/utils/resultParsers.ts
@@ -1,19 +1,43 @@
 import type { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
 export function parseDBInfo(result: PageObjectResponse[]) {
-  return result.map(
-    ({ properties, id, icon, created_time, created_by, archived, url }) => {
-      let title, emoji, user;
-      if (properties.Name.type === "title") {
-        title = properties.Name.title[0].plain_text;
-      }
-      if (icon?.type === "emoji") {
-        emoji = icon.emoji;
-      }
-      if (created_by.object === "user") {
-        user = created_by.id;
-      }
-      return { title, id, emoji, created_by, archived, created_time, url };
+  return result.map(({ properties, id, icon, created_time, archived, url }) => {
+    let title, emoji, user, rating, image, genre, color;
+    if (properties.Name.type === "title") {
+      title = properties.Name.title[0].plain_text;
     }
-  );
+
+    if (icon?.type === "emoji") {
+      emoji = icon.emoji;
+    }
+
+    if (properties.Rating.type === "rich_text") {
+      rating = properties.Rating.rich_text[0].plain_text;
+    }
+
+    if (properties.Person.type === "people") {
+      user = properties.Person.people[0].name;
+      image = properties.Person.people[0].avatar_url;
+    }
+
+    if (properties.Genre.type === "multi_select") {
+      genre = properties.Genre.multi_select[0].name;
+      color = properties.Genre.multi_select[0].color;
+    }
+
+    return {
+      properties,
+      title,
+      id,
+      emoji,
+      user,
+      archived,
+      created_time,
+      url,
+      rating,
+      image,
+      genre,
+      color,
+    };
+  });
 }
